03725663be9796f4f471df2b7e1015039b80ba6a
Juan Manuel Castro <97136502+Juanma1023@users.noreply.github.com>
2022-11-28 23:01:04 -0500

    Rutas para los usuarios

---
+++ App/v1/router/user_router.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import status
+from fastapi import Body
+
+from app.v1.schema import user_schema
+from app.v1.service import user_service
+
+
+from app.v1.utils.db import get_db
+
+
+router = APIRouter(prefix="/api/v1")
+
+@router.post(
+        "/user/",
+        tags=["users"],
+        status_code=status.HTTP_201_CREATED,
+        response_model=user_schema.User,
+        dependencies= [Depends(get_db)]
+    )
+def create_user(
+        user: user_schema.UserBase = Body(...)
+    ):
+        return user_service.create_user(user)
+
+@router.get(
+        "/user/{id}",
+        tags=["users"],
+        status_code=status.HTTP_200_OK,
+        response_model=user_schema.User,
+        dependencies= [Depends(get_db)]
+    )
+def get_one_user(
+        email: str
+    ):
+        return user_service.get_user(email)
+
+@router.put(
+    "/user/{id}/modificaciones",
+    tags=["users"],
+    status_code=status.HTTP_200_OK,
+    response_model=user_schema.User,
+    dependencies=[Depends(get_db)]
+)
+def modified_user(email: str, name: str, last_name: str, phone: int, address: str):
+    return user_service.update_user(email, name, last_name, phone, address)
+
+
+
+@router.delete("/user/delete/{id}", tags=["users"], status_code=status.HTTP_200_OK)
+def delete_user(id: int):
+    user_service.delete_user(id)
+    return {
+        'Msg': 'Usuario eliminado satisfactoramente'
+    }
+
+
